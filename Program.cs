cfa100c2f90ee28a0687562c375915df943aad66
a8962383 <ashfaq896nsu@gmail.com>
2021-01-28 16:55:35 +0100

    Private variable hack.

--- Program.cs
+++ Program.cs
@@ -256,6 +256,7 @@ using Newtonsoft.Json.Schema;
 // }
 #endregion
 
+#region Hack private variables
 // var myInstance = new MyClass();
 // var fieldInfo = typeof(MyClass).GetField("answer", BindingFlags.NonPublic | BindingFlags.Instance);
 // var answer = fieldInfo.GetValue(myInstance);
@@ -266,6 +267,8 @@ using Newtonsoft.Json.Schema;
 // {
 //     private int answer = 42;
 // }
+
+#endregion
 
 try {
     XDocument contacts = XDocument.Parse(
