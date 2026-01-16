bfbbbe9e1f2530bc488d6026ca1616cf89fdb86d
MDRN <mdrn@simcorp.com>
2026-01-16 15:51:53 +0100

    Refactor Complex struct to use private fields and improve operator overloads

--- Complex.cs
+++ Complex.cs
@@ -1,128 +1,132 @@
 using System;
 
-struct Complex
+public struct Complex
 {
-    float real;
-    float imaginary;
-    
+    private float _real;
+    private float _imaginary;
+
     public Complex(float real, float imaginary)
     {
-        this.real = real;
-        this.imaginary = imaginary;
-    }
-    
-    public float Real
-    {
-        get
-        {
-            return(real);
-        }
-        set
-        {
-            real = value;
-        }
-    }
-    
-    public float Imaginary
-    {
-        get
-        {
-            return(imaginary);
-        }
-        set
-        {
-            imaginary = value;
-        }
-    }
-    
-    public override string ToString()
-    {
-        return(String.Format("({0}, {1}i)", real, imaginary));
-    }
-    
-    public static bool operator==(Complex c1, Complex c2)
-    {
-        if ((c1.real == c2.real) &&
-        (c1.imaginary == c2.imaginary))
-        return(true);
-        else
-        return(false);
-    }
-    
-    public static bool operator!=(Complex c1, Complex c2)
-    {
-        return(!(c1 == c2));
-    }
-    
-    public override bool Equals(object o2)
-    {
-        Complex c2 = (Complex) o2;
-        
-        return(this == c2);
-    }
-    
-    public override int GetHashCode()
-    {
-        return(real.GetHashCode() ^ imaginary.GetHashCode());
-    }
-    
-    public static Complex operator+(Complex c1, Complex c2)
-    {
-        return(new Complex(c1.real + c2.real, c1.imaginary + c2.imaginary));
-    }
-    
-    public static Complex operator-(Complex c1, Complex c2)
-    {
-        return(new Complex(c1.real - c2.real, c1.imaginary - c2.imaginary));
-    }
-    
-    // product of two complex numbers
-    public static Complex operator*(Complex c1, Complex c2)
-    {
-        return(new Complex(c1.real * c2.real - c1.imaginary * c2.imaginary,
-        c1.real * c2.imaginary + c2.real * c1.imaginary));
-    }
-    
-    // quotient of two complex numbers
-    public static Complex operator/(Complex c1, Complex c2)
-    {
-        if ((c2.real == 0.0f) &&
-        (c2.imaginary == 0.0f))
-        throw new DivideByZeroException("Can't divide by zero Complex number");
-        
-        float newReal = 
-        (c1.real * c2.real + c1.imaginary * c2.imaginary) /
-        (c2.real * c2.real + c2.imaginary * c2.imaginary);
-        float newImaginary = 
-        (c2.real * c1.imaginary - c1.real * c2.imaginary) /
-        (c2.real * c2.real + c2.imaginary * c2.imaginary);
-        
-        return(new Complex(newReal, newImaginary));
-    }
-    
-    // non-operator versions for other languages
-    public static Complex Add(Complex c1, Complex c2)
-    {
-        return(c1 + c2);
-    }
-    
-    public static Complex Subtract(Complex c1, Complex c2)
-    {
-        return(c1 - c2);
-    }
-    
-    public static Complex Multiply(Complex c1, Complex c2)
-    {
-        return(c1 * c2);
-    }
-    
-    public static Complex Divide(Complex c1, Complex c2)
-    {
-        return(c1 / c2);
+        _real = real;
+        _imaginary = imaginary;
     }
 
+    public float Real
+    {
+        get => _real;
+        set => _real = value;
+    }
+
+    public float Imaginary
+    {
+        get => _imaginary;
+        set => _imaginary = value;
+    }
+
+    public override string ToString()
+    {
+        return string.Format("({0}, {1}i)", _real, _imaginary);
+    }
+
+    public static bool operator ==(Complex c1, Complex c2)
+    {
+        return c1._real == c2._real && c1._imaginary == c2._imaginary;
+    }
+
+    public static bool operator !=(Complex c1, Complex c2)
+    {
+        return !(c1 == c2);
+    }
+
+    public override bool Equals(object o2)
+    {
+        if (!(o2 is Complex c2))
+        {
+            return false;
+        }
+
+        return this == c2;
+    }
+
+    public override int GetHashCode()
+    {
+        return _real.GetHashCode() ^ _imaginary.GetHashCode();
+    }
+
+    public static Complex operator +(Complex c1, Complex c2)
+    {
+        return new Complex(c1._real + c2._real, c1._imaginary + c2._imaginary);
+    }
+
+    public static Complex operator -(Complex c1, Complex c2)
+    {
+        return new Complex(c1._real - c2._real, c1._imaginary - c2._imaginary);
+    }
+
+    /// <summary>
+    /// Product of two complex numbers.
+    /// </summary>
+    public static Complex operator *(Complex c1, Complex c2)
+    {
+        return new Complex(
+            c1._real * c2._real - c1._imaginary * c2._imaginary,
+            c1._real * c2._imaginary + c2._real * c1._imaginary);
+    }
+
+    /// <summary>
+    /// Quotient of two complex numbers.
+    /// </summary>
+    public static Complex operator /(Complex c1, Complex c2)
+    {
+        if (c2._real == 0.0f && c2._imaginary == 0.0f)
+        {
+            throw new DivideByZeroException("Can't divide by zero Complex number");
+        }
+
+        float denominator = c2._real * c2._real + c2._imaginary * c2._imaginary;
+        float newReal = (c1._real * c2._real + c1._imaginary * c2._imaginary) / denominator;
+        float newImaginary = (c2._real * c1._imaginary - c1._real * c2._imaginary) / denominator;
+
+        return new Complex(newReal, newImaginary);
+    }
+
+    /// <summary>
+    /// Non-operator version of addition.
+    /// </summary>
+    public static Complex Add(Complex c1, Complex c2)
+    {
+        return c1 + c2;
+    }
+
+    /// <summary>
+    /// Non-operator version of subtraction.
+    /// </summary>
+    public static Complex Subtract(Complex c1, Complex c2)
+    {
+        return c1 - c2;
+    }
+
+    /// <summary>
+    /// Non-operator version of multiplication.
+    /// </summary>
+    public static Complex Multiply(Complex c1, Complex c2)
+    {
+        return c1 * c2;
+    }
+
+    /// <summary>
+    /// Non-operator version of division.
+    /// </summary>
+    public static Complex Divide(Complex c1, Complex c2)
+    {
+        return c1 / c2;
+    }
+
+    /// <summary>
+    /// Calculates the magnitude of a complex number.
+    /// </summary>
     public static float Magnitude(Complex c)
     {
-        return (float)Math.Sqrt((c.real * c.real) + (c.imaginary * c.imaginary));
+        return (float)Math.Sqrt(c._real * c._real + c._imaginary * c._imaginary);
     }
 }
